3b1c893e8065bab69098bee85e9aa82c8ed38ab7
ysac <ysac@example.com>
2026-09-02 04:14:42 +0700

    fix: disable turbopack on Vercel

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    turbopack: !process.env.VERCEL, // Disable turbopack on Vercel temporarily
+  },
 };
 
 export default nextConfig;
